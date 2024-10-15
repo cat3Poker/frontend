b414cafaad0eba92545da750fe14e9bfb3e477c3
Highscoretech <highscoretechng@gmail.com>
2024-10-15 15:06:35 -0600

    cat3 poker is coming

--- src/routes/(logged-in)/gaming/+page.svelte
+++ src/routes/(logged-in)/gaming/+page.svelte
@@ -1,1 +1,4 @@
-<div>Gaming</div>
+<div class="sc-eCImPb cuPxwd empty full-abs">
+    <img alt="" src="https://static.nanogames.io/assets/empty.acd1f5fe.png">
+    <div class="msg">404, pPage is not ready yet! </div>
+</div>
---
+++ src/routes/(logged-in)/leaderboard/+page.svelte
@@ -0,0 +1,4 @@
+<div class="sc-eCImPb cuPxwd empty full-abs">
+    <img alt="" src="https://static.nanogames.io/assets/empty.acd1f5fe.png">
+    <div class="msg">404, pPage is not ready yet! </div>
+</div>
