097029ee37ca0debd6fc305cb8c86320e7a2f6ee
Highscoretech <highscoretechng@gmail.com>
2024-10-15 23:16:53 -0600

    cat3 poker is coming

--- src/lib/components/menubar.svelte
+++ src/lib/components/menubar.svelte
@@ -1,7 +1,9 @@
 <script>
     export let route
     import { goto } from "$app/navigation";
-
+    export let isCollabs 
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
     let Menu = [
         {icon: "icon_Home", route: "home"},
         {icon: "icon_leaderboard", route: "leaderboard"},
@@ -12,15 +14,22 @@
         {icon: "icon_daily-reward", route: "daily-reward"},
         {icon: "icon_Setting", route: "game-settings"},
     ]
+    
     const handleRoutes = ((e)=> {
         goto(`/${e}`)
     })
     $: routeEl = (route.url).replace("/", "")
 </script>
 
-<div class="menu-bar">
+<div class="menu-bar {isCollabs ? "collabs" : ""}">
     <div class="menu-bar-container">
-        {#each Menu as me}
+        <button on:click={()=> dispatch("change")} class="hONeioks">
+            <svg xmlns:xlink="http://www.w3.org/1999/xlink" class="sc-gsDKAQ hxODWG icon">
+                <use xlink:href="#icon_collabse"></use>
+            </svg>
+        </button>
+        <div class="Kjieoijos">
+            {#each Menu as me}
             <!-- svelte-ignore a11y-click-events-have-key-events -->
             <!-- svelte-ignore a11y-no-static-element-interactions -->
             <div on:click={()=> handleRoutes(me.route)} class="content {me.route === routeEl ? "active" : ""}">
@@ -29,10 +38,15 @@
                         <use xlink:href="#{me.icon}"></use>
                     </svg>
                 </div>
-                <div class="item-text">
-                    {(me.route).replace("-", " ")}
-                </div>
+                {#if !isCollabs}
+                    <div class="item-text">
+                        {(me.route).replace("-", " ")}
+                    </div>
+                {/if}
+             
             </div>
         {/each}
+        </div>
+   
     </div>
 </div>
--- src/routes/(logged-in)/+layout.svelte
+++ src/routes/(logged-in)/+layout.svelte
@@ -3,16 +3,16 @@
     import Footer from "$lib/components/footer.svelte";
     import Menubar from "$lib/components/menubar.svelte";
     import Navbar from "$lib/components/navbar.svelte";
+    $: isCollabs = false
 
 </script>
-
 
 <Navbar />
 
 <div class="default-bg">
     <div class="page-container">
-        <Menubar route={data}/>
-        <div class="default-width">
+        <Menubar {isCollabs} on:change={()=> isCollabs =! isCollabs} route={data}/>
+        <div class="default-width {isCollabs ? "collabs" : ""}">
             <slot></slot>
         </div>
         <Footer />
