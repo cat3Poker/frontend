6697cc88cb0b635b7c5bc78cbf71dc74bea6f43c
Highscoretech <highscoretechng@gmail.com>
2024-11-04 04:29:16 -0600

    send flop

--- src/routes/(auth)/username/+page.js
+++ src/routes/(auth)/username/+page.js
@@ -1,10 +1,13 @@
 import { browser } from '$app/environment';
-
-/** @type {import('./$types').PageLoad} */
-export async function load({ route }) {
+import { redirect } from '@sveltejs/kit';
+/** @type {import('./$types.js').PageLoad} */
+export async function load({  }) {
     let _preLogin = ""
     if(browser){
          _preLogin =  JSON.parse(sessionStorage.getItem("pre_login"))
+        if(!_preLogin){
+            throw redirect(303, `/signup`);
+        }
     }
     return {_preLogin}
 }
